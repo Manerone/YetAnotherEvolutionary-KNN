bfb462cda457dc8168b2ece214f07576970af033
MatheusAN <matheus.agio@hotmail.com>
2016-11-29 20:15:06 -0200

    Adding dataset_loader

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,17 +1,14 @@
-class BanknoteLoader:
+class DatasetLoader:
     def __init__(self, path):
         self.examples = []
         self.labels = []
         lines = self._read_lines(path)
         for line in lines:
-            self.examples.append(line[0:-1])
+            self.examples.append(list(map(float, line[0:-1])))
             self.labels.append(line[-1])
 
     def _read_lines(self, path):
         lines = []
         for line in open(path):
-            array = line.rstrip('\n').split(',')
-            lines.append(
-                list(map(float, array))
-            )
+            lines.append(line.rstrip('\n').split(','))
         return lines
